9b030281b6753e46a68430d888ee5ff3fa1319cb
MoCrespo <mcrespo495@gmail.com>
2024-03-04 22:30:34 +0200

    Create userSchema had unique username and firstname, lastname password , date have a default value date.now

---
+++ server/src/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+export const User = mongoose.model("User", UserSchema);

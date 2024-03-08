f89de7c133d5ace9fd6a268ef1476bf324dec125
MoCrespo <mcrespo495@gmail.com>
2024-03-08 19:34:21 +0200

    test main endpoint

---
+++ server/src/tests/index.spec.ts
@@ -0,0 +1,12 @@
+import supertest from "supertest";
+import app from '../index'
+
+// create a request object 
+const request = supertest(app);
+
+describe('Test main endpoint', () => {
+    it('server running correctly ', async () => {
+        const response  = await request.get('/')
+        expect(response.status).toBe(200);
+    })
+})

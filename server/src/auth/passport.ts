b209a4fe702ccba5021281d1c148c6b1bebe6d57
MoCrespo <mcrespo495@gmail.com>
2024-03-09 06:13:58 +0200

    create a passport config file to handel auth with login

---
+++ server/src/auth/passport.ts
@@ -0,0 +1,42 @@
+import { PassportStatic } from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+import { checkUserExistence , verifyPassword } from '../services/login.service';
+
+
+const authenticateUser = async (username: string, password: string, done) => {
+  try {
+   
+    const isValid = await verifyPassword(username, password);
+
+    if (isValid) {
+      
+      return done(null, username );
+    } else {
+     
+      return done(null, false, { message: 'Invalid username or password' });
+    }
+  } catch (error) {
+   
+    return done(error);
+  }
+};
+
+
+export const initialize = (passport: PassportStatic) => {
+  passport.use(new LocalStrategy({ usernameField: 'username' }, authenticateUser));
+  passport.serializeUser((user: {username: string}, done) => {
+    done(null, user.username)
+  })
+  passport.deserializeUser(async (username: string, done) => {
+    try{
+        const user = await checkUserExistence(username)
+        if(user) {
+            done(null, user)
+        } else {
+            done(new Error('User not found'))
+        }
+    } catch(error) {
+        done(error)
+    }
+  })
+};

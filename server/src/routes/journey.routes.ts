5e965be592e7c7d380cc23abe30ec24efecb790f
MoCrespo <mcrespo495@gmail.com>
2024-03-13 19:08:28 +0200

    create journeyRoutes

--- server/src/routes/journey.routes.ts
+++ server/src/routes/journey.routes.ts
@@ -1,14 +1,18 @@
 import express from 'express'
-import { createJourney, deleteJourney, getJourneyById, getaAllJourneys, updateJourney } from '../controllers/journey.controller'
+import { createJourney, dashboardView, deleteJourney, editJourneyView, getJourneyById, newJourneyView, updateJourney } from '../controllers/journey.controller'
+import { checkAuthenticated } from '../middleware/auth.middleware'
+
 
 const journeyRoutes = (app: express.Application) => {
-    app.get('/journey', getaAllJourneys)
-    app.get('/journey/:id', getJourneyById)
+    app.get('/', checkAuthenticated, dashboardView)
+    app.get('/journey/new', checkAuthenticated ,newJourneyView)
+    app.get('/journey/:id',checkAuthenticated, getJourneyById)
+    app.get('/journey/edit/:id', checkAuthenticated, editJourneyView)
 
-    app.post('/journey', createJourney)
-    app.patch('/journey/:id', updateJourney)
+    app.post('/journey', checkAuthenticated ,createJourney)
+    app.patch('/journey/:id', checkAuthenticated , updateJourney)
 
-    app.delete('/journey/:id', deleteJourney)
+    app.delete('/journey/:id', checkAuthenticated, deleteJourney)
 }
 
 export default journeyRoutes
